0ddf433504a12615bec6eba27f4c9999ba1c39f7
Charles Fenwick Elliott <Charles@FenwickElliott.io>
2018-01-17 14:36:16 -0500

    Getting code

--- main.go
+++ main.go
@@ -1,10 +1,12 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
+	"io"
 	"io/ioutil"
+	"net/http"
 	"os"
+	"os/exec"
 	"path"
 )
 
@@ -21,7 +23,6 @@ type userData struct {
 func main() {
 	userRaw, err := ioutil.ReadFile(db)
 	if os.IsNotExist(err) {
-		fmt.Println("no such file")
 		initialize()
 	} else {
 		check(err)
@@ -29,15 +30,31 @@ func main() {
 	fmt.Println(userRaw)
 }
 
-func initialize() {
-	user := userData{"Access", "Refresh", "UID", "PID"}
-	userJSON, err := json.Marshal(user)
-	check(err)
-	fmt.Println(string(userJSON))
-}
-
 func check(err error) {
 	if err != nil {
 		panic(err)
 	}
+}
+
+func initialize() {
+	fmt.Println("Initializing...")
+	done := make(chan bool)
+	go serve(done)
+	exec.Command("open", "https://accounts.spotify.com/authorize/?client_id=715c15fc7503401fb136d6a79079b50c&response_type=code&redirect_uri=http://localhost:3456/catch&scope=user-read-playback-state%20playlist-read-private%20playlist-modify-private").Start()
+
+	finished := <-done
+
+	if finished {
+		fmt.Println("Initiation complete")
+	}
+}
+
+func serve(done chan bool) {
+	http.HandleFunc("/catch", func(w http.ResponseWriter, r *http.Request) {
+		io.WriteString(w, "Thank you, GoSnatch can now access your spotify account.\nYou may close this window.\n")
+		code := r.URL.Query()["code"][0]
+		fmt.Println(code)
+		done <- true
+	})
+	http.ListenAndServe(":3456", nil)
 }
